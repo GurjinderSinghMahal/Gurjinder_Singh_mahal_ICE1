c9784e4571f126d853634102e4a232ebe26d9f01
GurjinderSinghMahal <50635244+GurjinderSinghMahal@users.noreply.github.com>
2019-05-22 00:54:29 -0400

    Update CardTrick.java

--- NetBeansProjects/Spring_2019/PickACard/src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ NetBeansProjects/Spring_2019/PickACard/src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
-
+import java.util.Scanner;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -21,10 +21,30 @@ public class CardTrick {
         {
             Card c = new Card();
             //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue(c.getRandomValue());
+            
+            //c.setSuit(Card.Suits(insert call to random number between 0-3 here)
+            c.setSuit(Card.SUITS[c.getRandomSuit()]);
+            
+            magicHand[i] = c;
         }
         
         //insert code to ask the user for Card value and suit, create their card
+         Scanner input = new Scanner(System.in);
+        System.out.println("Enter the value of suit :");
+        String suit = input.nextLine();
+        
+        System.out.println("Enter numbr btween 1 to 13");
+        int number = input.nextInt();
+        
+        for(int num = 0; num< magicHand.length; num++) {
+            if(suit.equals(magicHand[num].getSuit()) && number == magicHand[num].getValue()) {
+                System.out.println("FOUND");
+            }
+            else {
+                System.out.println("NOT FOUND");
+            }
+        }
         // and search magicHand here
         //Then report the result here
     }
